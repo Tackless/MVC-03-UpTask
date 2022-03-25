71e432b31c8a11d243ef407cca2fcaf25ae7315e
Donovan Eliosa <oswaldo.d.b.e@hotmail.com>
2022-03-24 23:02:31 -0600

    validar usuario

--- controllers/LoginController.php
+++ controllers/LoginController.php
@@ -30,6 +30,18 @@ class LoginController {
         if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $usuario->sincronizar($_POST);
             $alertas = $usuario->validarNuevaCuenta();
+
+            if (empty($alertas)) {
+                $existeUsuario = Usuario::where('email', $usuario->email);
+
+                if ($existeUsuario) {
+                    Usuario::setAlerta('error', 'El Usuario ya está registrado');
+                    $alertas = Usuario::getAlertas();
+                } else {
+                    
+                }
+            }
+            
         }
 
         $router->render('auth/crear', [
