fca6b60e94141fd115b03baad34205c6913a85be
Donovan Eliosa <oswaldo.d.b.e@hotmail.com>
2022-03-24 22:18:48 -0600

    add validate password

--- models/Usuario.php
+++ models/Usuario.php
@@ -12,6 +12,7 @@ class Usuario extends ActiveRecord {
         $this->nombre = $args['nombre'] ?? '';
         $this->email = $args['email'] ?? '';
         $this->password = $args['password'] ?? '';
+        $this->password2 = $args['password2'] ?? '';
         $this->token = $args['token'] ?? '';
         $this->confirmado = $args['confirmado'] ?? '';
     }
@@ -24,6 +25,18 @@ class Usuario extends ActiveRecord {
         
         if (!$this->email) {
             self::$alertas['error'][] = 'El E-mail de Usuario es Obligatorio';
+        }
+
+        if (!$this->password) {
+            self::$alertas['error'][] = 'El Password no puede ir vacío';
+        }
+
+        if (strlen($this->password) < 6 ) {
+            self::$alertas['error'][] = 'El Password debe contener al menos 6 carácteres';
+        }
+
+        if ($this->password !== $this->password2) {
+            self::$alertas['error'][] = 'Los Passwords son diferentes';
         }
 
         return self::$alertas;
