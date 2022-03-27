e724a30bd62afb9ebca0672aa59215d62648e34a
Donovan Eliosa <oswaldo.d.b.e@hotmail.com>
2022-03-27 12:57:18 -0600

    validate new project

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -2,7 +2,9 @@
 
 namespace Controllers;
 
+use Model\Proyecto;
 use MVC\Router;
+
 
 class DashboardController {
     public static function index(Router $router) {
@@ -20,10 +22,19 @@ class DashboardController {
     public static function crear_proyecto(Router $router) {
 
         session_start();
-
         isAuth();
-
         $alertas = [];
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $proyecto = new Proyecto($_POST);
+
+            // Validación
+            $alertas = $proyecto->validarProyecto();
+
+            if (empty($alertas)) {
+                
+            }
+        }
 
         $router->render('dashboard/crear-proyecto', [
             'titulo' => 'Crear Proyecto', 
